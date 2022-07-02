df43ac89241294859cd82ff0644eeb577d5eed33
Nya Ξlimu <info@elimu.ai>
2022-07-02 09:58:03 +0300

    Update 1_deploy_contract.js

--- community-token/migrations/1_deploy_contract.js
+++ community-token/migrations/1_deploy_contract.js
@@ -1,5 +1,11 @@
 const ElimuToken = artifacts.require("ElimuToken");
+const fs = require('fs');
 
 module.exports = function (deployer) {
   deployer.deploy(ElimuToken);
+
+  // Save ABI
+  const contract = JSON.parse(fs.readFileSync('./build/contracts/ElimuToken.json', 'utf8'));
+  console.log('Saving ABI:', './abis/ElimuToken.json.abi')
+  fs.writeFileSync('../abis/ElimuToken.json.abi', JSON.stringify(contract.abi))
 };
