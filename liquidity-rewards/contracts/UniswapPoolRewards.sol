8094dea317d116abe1a15043493bccddc2220bca
Homayoun <hhio618@gmail.com>
2022-07-03 13:26:18 +0430

    Update liquidity-rewards/contracts/UniswapPoolRewards.sol
    Co-authored-by: Nya Ξlimu <info@elimu.ai>

--- liquidity-rewards/contracts/UniswapPoolRewards.sol
+++ liquidity-rewards/contracts/UniswapPoolRewards.sol
@@ -8,7 +8,6 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
-
 contract UniswapPoolRewards is PoolTokenWrapper, Ownable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
