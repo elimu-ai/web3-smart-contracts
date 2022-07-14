bffee9ae03b4ce9558addea8cc4c8c1e6dd7856b
Homayoun <hhio618@gmail.com>
2022-07-14 18:54:38 +0430

    Update liquidity-rewards/test/UniswapPoolRewardsTest.js
    Co-authored-by: Nya Ξlimu <info@elimu.ai>

--- liquidity-rewards/test/UniswapPoolRewardsTest.js
+++ liquidity-rewards/test/UniswapPoolRewardsTest.js
@@ -64,7 +64,7 @@ contract('UniswapPoolRewards', function ([_, wallet1, wallet2, wallet3, wallet4,
       // Mint and transfer elimu tokens.
       await that.elimu.mint(owner, web3.utils.toWei('10_000_000'));
       await that.elimu.approve(that.pool.address, new BN(2).pow(new BN(255)), { from: owner });
-      await that.pool.depositReward(web3.utils.toWei('10000000'), { from: owner });
+      await that.pool.depositReward(web3.utils.toWei('10_000_000'), { from: owner });
 
       await that.uni.mint(wallet1, web3.utils.toWei('1_000'));
       await that.uni.mint(wallet2, web3.utils.toWei('1_000'));
