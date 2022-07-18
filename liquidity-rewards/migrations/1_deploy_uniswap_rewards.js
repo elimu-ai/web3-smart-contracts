542a73ed52e23df143277c9b70f62302aa97921b
aahna-ashina <info@elimu.ai>
2022-07-18 14:37:41 +0800

    test: #38 Deploy Uniswap rewards to Rinkeby
    Set token addresses.

--- liquidity-rewards/migrations/1_deploy_uniswap_rewards.js
+++ liquidity-rewards/migrations/1_deploy_uniswap_rewards.js
@@ -2,9 +2,10 @@ const UniswapPoolRewards = artifacts.require("UniswapPoolRewards");
 const fs = require('fs');
 
 module.exports = function (deployer, network) {
+  console.log('deployer.options.from:', deployer.options.from);
   console.log('network:', network);
 
-  let isDryRun = network.endsWith('-fork') || network.endsWith('coverage');
+  const isDryRun = network.endsWith('-fork') || network.endsWith('coverage');
   if (!isDryRun) {
     // Save the contract's ABI
     const contractJson = JSON.parse(fs.readFileSync('./build/contracts/' + UniswapPoolRewards.contractName + '.json', 'utf8'));
@@ -12,7 +13,17 @@ module.exports = function (deployer, network) {
     fs.writeFileSync('./abis/' + network + '/' + UniswapPoolRewards.contractName + '.json.abi', JSON.stringify(contractJson.abi));
   }
 
-  var ELIMUTokenAddress = process.env.ELIMU_TOKEN_ADDRESS || '0xe29797910d413281d2821d5d9a989262c8121cc2';
-  var UniswapTokenAddress = process.env.UNISWAP_LP_TOKEN_ADDRESS || '0xa0d230dca71a813c68c278ef45a7dac0e584ee61';
-  deployer.deploy(UniswapPoolRewards, ELIMUTokenAddress, UniswapTokenAddress);
+  if (network == 'ganache') {
+    const elimuToken = '0x0000000000000000000000000000000000000000';
+    console.log('elimuToken:', elimuToken);
+    const poolToken = '0x0000000000000000000000000000000000000000';
+    console.log('poolToken:', poolToken);
+    deployer.deploy(UniswapPoolRewards, elimuToken, poolToken);
+  } else if (network == 'rinkeby') {
+    const elimuToken = '0xe29797910d413281d2821d5d9a989262c8121cc2';
+    console.log('elimuToken:', elimuToken);
+    const poolToken = '0x9936bdcd16e8c709c4cb8d7b871f0011b4cc65de';
+    console.log('poolToken:', poolToken);
+    deployer.deploy(UniswapPoolRewards, elimuToken, poolToken);
+  }
 };
