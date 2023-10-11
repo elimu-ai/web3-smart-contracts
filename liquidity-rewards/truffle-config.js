582dcbdf88f71e95e8fdf7b83bde021090069afb
Bender-22 <85234598+Bender-22@users.noreply.github.com>
2023-10-11 21:58:19 +0700

    goerli deployment
    #101

--- liquidity-rewards/truffle-config.js
+++ liquidity-rewards/truffle-config.js
@@ -26,6 +26,7 @@ module.exports = {
     goerli: {
       provider: () => new HDWalletProvider(mnemonic, 'https://rpc.ankr.com/eth_goerli'),
       network_id: 5,
+      networkCheckTimeout: 60_000,
       gas: 2_500_000,
       gasPrice: 4_000_000_000, // 4 gwei (in wei)
       timeoutBlocks: 200
