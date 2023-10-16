88e919b3f95572ac0699a08ecbbca1de9dac3396
Bender-22 <85234598+Bender-22@users.noreply.github.com>
2023-10-16 15:05:03 +0700

    balancer pool rewards
    adjust rate per second

--- liquidity-rewards/contracts/BalancerPoolRewards.sol
+++ liquidity-rewards/contracts/BalancerPoolRewards.sol
@@ -11,7 +11,7 @@ contract BalancerPoolRewards is IPoolRewards, AccessControl {
     IERC20 public rewardToken;
     IERC20 public poolToken;
 
-    uint256 public rewardRatePerSecond = 0.199 * 1e18; // ~516,000 per month
+    uint256 public rewardRatePerSecond = 0.249 * 1e18; // ~645,000 per month
     uint256 public lastRewardPerPoolToken;
     uint256 public lastUpdateTimestamp;
 
