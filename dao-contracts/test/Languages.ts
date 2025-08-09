c5f704999d8fb8492cfb7a6f82705a93f86d0979
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-08-09 17:14:56 +0700

    test: deployment
    #107

---
+++ dao-contracts/test/Languages.ts
@@ -0,0 +1,32 @@
+import {
+  loadFixture,
+} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+import hre from "hardhat";
+
+describe("Languages", function () {
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshot in every test.
+  async function deployFixture() {
+    // Contracts are deployed using the first signer/account by default
+    const [owner, otherAccount] = await hre.ethers.getSigners();
+
+    const Languages = await hre.ethers.getContractFactory("Languages");
+    const languages = await Languages.deploy();
+
+    return { languages, owner, otherAccount };
+  }
+
+  describe("Deployment", function () {
+    it("Should not set any supported languages", async function () {
+      const { languages } = await loadFixture(deployFixture);
+
+      expect(await languages.isSupportedLanguage("ENG")).to.equal(false);
+      expect(await languages.isSupportedLanguage("HIN")).to.equal(false);
+      expect(await languages.isSupportedLanguage("TGL")).to.equal(false);
+      expect(await languages.isSupportedLanguage("THA")).to.equal(false);
+      expect(await languages.isSupportedLanguage("VIE")).to.equal(false);
+    });
+  });
+});
