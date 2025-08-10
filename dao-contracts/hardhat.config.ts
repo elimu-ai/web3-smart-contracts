e6faa0f9617a039ce1aae14e2f875699e9536525
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-08-10 11:07:52 +0700

    build: enable gas optimizer
    #107

--- dao-contracts/hardhat.config.ts
+++ dao-contracts/hardhat.config.ts
@@ -2,7 +2,14 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true
+      }
+    }
+  },
   gasReporter: {
     enabled: true,
     outputFile: "hardhat-gas-report.md"
